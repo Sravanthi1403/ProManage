bab288800b626a0ba230ccd88ffcb488d10baf15
A Sravanthi <rsravanthi555@gmail.com>
2024-03-05 19:27:30 +0530

    made changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const errorMiddleware = require("./middlewares/error-middleware");
 const cors = require("cors");
 
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "https://promanagefrontend-1v25gad2p-sravanthis-projects-bdbcceab.vercel.app",
     methods: "GET,POST,PUT,DELETE,PATCH,HEAD",
     credentials: true,
 };
